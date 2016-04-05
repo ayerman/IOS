fbd0068283aa6161994c125b1ce52c9c7e0c0c7c
ayerman <yermanaaron@Aarons-iMac.local>
2016-04-05 19:01:25 -0400

    initial commit

---
+++ Personal Trainer/Personal Trainer/ResultsController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsController.swift
+//  Personal Trainer
+//
+//  Created by Aaron Yerman on 4/5/16.
+//  Copyright © 2016 Aaron Yerman. All rights reserved.
+//
+
+import UIKit
+
+class ResultsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
